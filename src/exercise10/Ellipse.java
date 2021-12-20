084fec0b2678c662d7e4b09afe16f6223e12f70a
NNikolov02 <>
2021-12-20 22:34:36 +0000

    Ubung10

--- src/exercise10/Ellipse.java
+++ src/exercise10/Ellipse.java
@@ -1,37 +1,35 @@
 package exercise10;
 
 public class Ellipse extends Figure {
-    
-    public Ellipse() {
-    super(new Point(0, 0),  1, 1);
+   public Ellipse() {
+        super(new Point(0, 0),  1, 1);
     }
 
     public Ellipse(Point startPoint, double a, double b) {
-      super(startPoint ,a ,b);
-         }
-
-    public Ellipse(Ellipse otherEllipse) {
-        
-        super(otherEllipse.startPoint , otherEllipse.side1 , otherEllipse.side2);
+        super(startPoint, a, b);
     }
 
-    
-@Override
+    public Ellipse(Ellipse otherEllipse) {
+        super(new Point(otherEllipse.startPoint), otherEllipse.side1, otherEllipse.side2);
+    }
+
+   @Override
     public double calculatePerimeter() {
         return Math.PI * (3.0 * (side1 + side2) - Math.sqrt((3.0 * side1 + side2) * (side1 + 3.0 * side2)));
     }
-@Override
+
+    @Override
     public double calculateArea() {
         return Math.PI * side1 * side2;
     }
-@Override
+
+    @Override
     public String getType() {
         return (side1 == side2) ? "Circle" : "Ellipse";
     }
 
-   
-@Override
-   public boolean equal(Figure figure) {
+    @Override
+    public boolean equal(Figure figure) {
         boolean sameA = Utils.equals(side1, figure.side1);
         boolean sameB = Utils.equals(side2, figure.side2);
         boolean sameAReversed = Utils.equals(side1, figure.side2);
@@ -39,14 +37,13 @@ public class Ellipse extends Figure {
 
         return (sameA && sameB) || (sameAReversed && sameBReversed);
     }
-    
-@Override
+
+    @Override
     public boolean containsClick(Point click) {
-         if ( ((click.getX() - startPoint.getX()) / Math.pow(side1, 2)) -
+        if ( ((click.getX() - startPoint.getX()) / Math.pow(side1, 2)) -
             ((click.getY() - startPoint.getY()) / Math.pow(side2, 2)) <= 1) {
             return true;
         }
         return false;
     }
 }
-      
